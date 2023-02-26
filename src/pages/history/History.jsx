2a59501ca21b7e484392558933703c6a12cc790b
nurzhana <nurzhana@MacBook-Air-admin.local>
2023-02-26 18:18:22 +0300

    history page added

---
+++ src/pages/history/History.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { NavBar } from '../../components/nav/NavBar';
+
+export default function History() {
+    return (
+        <div>
+            <NavBar />
+            history
+        </div>
+    )
+}
